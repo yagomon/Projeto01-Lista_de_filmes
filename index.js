3c83b27cc02337ad5b73f0567d1c2bb1d89768df
yago.monteiroo@gmail.com <yago.monteiroo@gmail.com>
2021-08-18 23:02:54 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.use(express.json()); //falar para os reqs do express trabalahr com json
+
+const filmes = [
+    'Capitão America: O preimeiro vingador',
+    'Capitã Marvel',
+    'O Incrível Hulk',
+    'Homem de Ferro',
+    'Homem de Ferro 2',
+]
+
+app.get('/', (req,res)=>{
+    res.send('Hello Bluemers');
+});
+
+app.get('/filmes', (req,res)=>{
+    res.send(filmes)
+});
+
+app.get('/filmes/:id', (req,res) => {
+    const id = req.params.id -1;
+
+    const filme = filmes[id];
+
+    if(!filme) {
+        res.send('Filme não encontrado!')
+    };
+
+    res.send(filme)
+});
+
+// ROTA QUE CADASTRA UM NOVO FILME:
+// read => get
+// criar => post
+// update => put
+// delete => delete
+
+app.post('/filmes', (req,res) => {
+    const filme = req.body.filme;
+    const id = filmes.length;
+    filmes.push(filme);
+
+    res.send(`Filme adicionado com sucesso ${filme}.`)
+});
+
+
+// Utilizando o put (update)
+app.put('/filmes/:id', (req,res) => {
+    const id = req.params.id -1;
+    const filmeAntigo = filmes[id];
+    const filme = req.body.filme;
+    filmes[id]= filme;
+    res.send(`Filme ${filmeAntigo} foi atualizado para ${filme} com sucesso!`)
+})
+
+//Fazendo um delete
+app.delete('/filmes/:id', (req, res) => {
+    const id = req.params.id -1;
+    const filmeAexcluir = filmes[id]
+
+    if(!filmeAexcluir) {
+        res.send('Filme não encontrado!')
+    }
+    
+    delete filmes;
+    res.send(`O filme ${filmeAexcluir} foi apagado da lista.`)
+
+});
+
+//sugestao da galera SPLICE (remove da lista sem deixar o null)
+app.delete('/filmesSplice/:id', (req,res)=>{
+    const id = req.params.id-1;
+    filmes.splice(id,1) // deleta a partir de qual, quantos vai deletar
+    res.send("Filme excluido com sucesso.")
+  });
+  
+
+
+
+app.listen(port, () => {
+    console.info(`O app esta rodando na porta http://localhost:${port}`)
+})
+
+
